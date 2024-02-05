c7a0369bbff7b9d602752f1bbdd0d7cf6391f5e3
ivanberez <berejnoyi@mail.ru>
2024-02-05 16:32:12 +0800

    Добавление новых спрайтов

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     [SerializeField] private float _speed = 2;
+    [SerializeField] private Vector2[] _pointsWay;
 
     private Vector2 _direction = Vector2.zero;
 
@@ -11,7 +12,7 @@ public class Movement : MonoBehaviour
         var targetPosition = (Vector2)transform.position + _direction;
         transform.position = Vector2.MoveTowards(transform.position, targetPosition, _speed * Time.deltaTime);
     }
-
+    
     public void SetDirection(Vector2 direction)
     {
         _direction = direction;
